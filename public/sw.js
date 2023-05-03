8e031c8a106acddd81d2b5e05615437017e503e2
kwjung <kwjung@businessinsight.co.kr>
2023-05-03 12:44:48 +0900

    test

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,20 @@ importScripts(
 // code in this file, above the `precacheAndRoute` line.
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+
+
+self.addEventListener("push", (event) => {
+    const payload = JSON.parse(event.data.text());
+    event.waitUntil(
+      registration.showNotification(payload.title, {
+        body: payload.body,
+        data: { link: payload.link },
+      })
+    );
+  });
+  self.addEventListener("notificationclick", (event) => {
+    clients.openWindow(event.notification.data.link);
+  });
+  self.addEventListener("install", () => {
+    self.skipWaiting();
+  });
